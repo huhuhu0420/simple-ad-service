92812d0da6dc0d5e7108da9e1ff51dd36e98e6e6
huhuhu0420 <ghostcheng.00@gmail.com>
2024-02-19 16:47:22 +0800

    Update AdInfo and Conditions models in swagger.yaml and ads.go

--- backend/domain/ads.go
+++ backend/domain/ads.go
@@ -7,8 +7,8 @@ import (
 // Ad represents an advertisement information
 // @Description A brief advertisement information
 type Ad struct {
-	Title string `json:"title"`
-	EndAt string `json:"endAt"`
+	Title string `json:"title" example:"Ad title"`
+	EndAt string `json:"endAt" example:"2023-12-01T00:00:00Z" format:"date-time"`
 }
 
 // AdsResponse represents the response structure containing multiple ads
@@ -20,31 +20,31 @@ type AdsResponse struct {
 // Conditions represents the conditions for getting ads
 // @Description The conditions for getting ads
 type Conditions struct {
-	AgeStart int      `json:"ageStart"`
-	AgeEnd   int      `json:"ageEnd"`
-	Country  []string `json:"country"`
-	Platform []string `json:"platform"`
-	Gender   []string `json:"gender"`
+	AgeStart int      `json:"ageStart" example:"18"`
+	AgeEnd   int      `json:"ageEnd" example:"65"`
+	Country  []string `json:"country" example:"US"`
+	Platform []string `json:"platform" example:"Android"`
+	Gender   []string `json:"gender" example:"M"`
 }
 
 // AdInfo represents an advertisement information
 // @Description An advertisement information
 type AdInfo struct {
-	Title      string     `json:"title"`
-	StartAt    string     `json:"startAt"`
-	EndAt      string     `json:"endAt"`
+	Title      string     `json:"title" binding:"required" example:"Ad title"`
+	StartAt    string     `json:"startAt" binding:"required" example:"2023-01-01T00:00:00Z" format:"date-time"`
+	EndAt      string     `json:"endAt" binding:"required" example:"2023-12-01T00:00:00Z" format:"date-time"`
 	Conditions Conditions `json:"conditions,omitempty"`
 }
 
 // SearchAdRequest represents the request structure for searching ads
 // @Description The request structure for searching ads
 type SearchAdRequest struct {
-	Offset   int    `json:"offset"`
-	Limit    int    `json:"limit"`
-	Age      int    `json:"age"`
-	Gender   string `json:"gender"`
-	Country  string `json:"country"`
-	Platform string `json:"platform"`
+	Offset   int    `json:"offset" example:"0"`
+	Limit    int    `json:"limit" example:"10"`
+	Age      int    `json:"age" example:"18"`
+	Gender   string `json:"gender" example:"M"`
+	Country  string `json:"country" example:"US"`
+	Platform string `json:"platform" example:"Android"`
 }
 
 type AdService interface {
@@ -54,5 +54,6 @@ type AdService interface {
 
 type AdRepository interface {
 	CreateAd(ad AdInfo, conditions Conditions) error
+	InsertNewAd(title string, startAt string, endAt string) (int, error)
 	GetAd(searchAdRequest SearchAdRequest) (*AdsResponse, error)
 }
