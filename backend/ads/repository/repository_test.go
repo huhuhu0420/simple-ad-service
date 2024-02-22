9879d9d7600adacae56daa7ebd35bf9ecb821221
huhuhu0420 <ghostcheng.00@gmail.com>
2024-02-22 19:20:48 +0800

    use testify in ad repository unit tests

--- backend/ads/repository/repository_test.go
+++ backend/ads/repository/repository_test.go
@@ -1,136 +1,114 @@
 package repository
 
 import (
+	"database/sql"
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/huhuhu0420/simple-ad-service/domain"
+	"github.com/stretchr/testify/suite"
 )
 
-func TestMain(m *testing.M) {
-	m.Run()
+type repositorySuite struct {
+	suite.Suite
+	mock       sqlmock.Sqlmock
+	repository domain.AdRepository
 }
 
-func TestInsertNewAd(t *testing.T) {
-	db, mock, err := sqlmock.New()
+func (s *repositorySuite) SetupTest() {
+	db := &sql.DB{}
+	var err error
+	db, s.mock, err = sqlmock.New()
 	if err != nil {
-		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+		s.T().Fatal(err)
 	}
-	defer db.Close()
+	s.repository = NewAdRepository(db)
+}
 
-	mock.ExpectQuery("INSERT INTO ads").
+func (s *repositorySuite) TearDownTest() {
+	s.repository.(*adRepository).db.Close()
+}
+
+func (s *repositorySuite) TestInsertNewAd() {
+	s.mock.ExpectQuery("INSERT INTO ads").
 		WithArgs("test", "2021-01-01", "2021-01-02").
 		WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(1))
 
-	r := NewAdRepository(db)
-	id, err := r.InsertNewAd("test", "2021-01-01", "2021-01-02")
-	if err != nil {
-		t.Errorf("error was not expected while updating stats: %s", err)
-	}
-	if id != 1 {
-		t.Errorf("expected id to be 1, but got %d", id)
-	}
+	id, err := s.repository.InsertNewAd("test", "2021-01-01", "2021-01-02")
+	s.NoError(err)
+	s.Assert().Equal(1, id)
 }
 
-func TestInsertAgeRange(t *testing.T) {
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
-	}
-	defer db.Close()
-
-	mock.ExpectExec("INSERT INTO ad_ages").
-		WithArgs(1, 1, 100).
+func (s *repositorySuite) TestInsertAgeRange() {
+	s.mock.ExpectExec("INSERT INTO ad_ages").
+		WithArgs(1, 10, 20).
 		WillReturnResult(sqlmock.NewResult(1, 1))
 
-	r := NewAdRepository(db)
-	err = r.InsertAgeRange(1, 1, 100)
-	if err != nil {
-		t.Errorf("error was not expected while updating stats: %s", err)
-	}
+	err := s.repository.InsertAgeRange(1, 10, 20)
+	s.NoError(err)
 }
 
-func TestInsertCountry(t *testing.T) {
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
-	}
-	defer db.Close()
-
-	mock.ExpectExec("INSERT INTO ad_countries").
+func (s *repositorySuite) TestInsertCountry() {
+	s.mock.ExpectExec("INSERT INTO ad_countries").
 		WithArgs(1, "US").
 		WillReturnResult(sqlmock.NewResult(1, 1))
 
-	r := NewAdRepository(db)
-	err = r.InsertCountry(1, []string{"US"})
-	if err != nil {
-		t.Errorf("error was not expected while updating stats: %s", err)
-	}
+	err := s.repository.InsertCountry(1, []string{"US"})
+	s.NoError(err)
 }
 
-func TestInsertPlatform(t *testing.T) {
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
-	}
-	defer db.Close()
-
-	mock.ExpectExec("INSERT INTO ad_platforms").
+func (s *repositorySuite) TestInsertPlatform() {
+	s.mock.ExpectExec("INSERT INTO ad_platforms").
 		WithArgs(1, "ios").
 		WillReturnResult(sqlmock.NewResult(1, 1))
 
-	r := NewAdRepository(db)
-	err = r.InsertPlatform(1, []string{"ios"})
-	if err != nil {
-		t.Errorf("error was not expected while updating stats: %s", err)
-	}
+	err := s.repository.InsertPlatform(1, []string{"ios"})
+	s.NoError(err)
 }
 
-func TestInsertGender(t *testing.T) {
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
-	}
-	defer db.Close()
+func (s *repositorySuite) TestInsertGender() {
+	s.mock.ExpectExec("INSERT INTO ad_genders").
+		WithArgs(1, "M").
+		WillReturnResult(sqlmock.NewResult(1, 1))
+	err := s.repository.InsertGender(1, []string{"M"})
+	s.NoError(err)
+}
 
-	mock.ExpectExec("INSERT INTO ad_genders").
+func (s *repositorySuite) TestCreateAd() {
+	s.mock.ExpectQuery("INSERT INTO ads").
+		WithArgs("test", "2021-01-01", "2021-01-02").
+		WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(1))
+
+	s.mock.ExpectExec("INSERT INTO ad_ages").
+		WithArgs(1, 10, 20).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	s.mock.ExpectExec("INSERT INTO ad_countries").
+		WithArgs(1, "US").
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	s.mock.ExpectExec("INSERT INTO ad_platforms").
+		WithArgs(1, "ios").
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	s.mock.ExpectExec("INSERT INTO ad_genders").
 		WithArgs(1, "M").
 		WillReturnResult(sqlmock.NewResult(1, 1))
 
-	r := NewAdRepository(db)
-	err = r.InsertGender(1, []string{"M"})
-	if err != nil {
-		t.Errorf("error was not expected while updating stats: %s", err)
-	}
+	err := s.repository.CreateAd(domain.AdInfo{
+		Title:   "test",
+		StartAt: "2021-01-01",
+		EndAt:   "2021-01-02",
+	}, domain.Conditions{
+		AgeStart: 10,
+		AgeEnd:   20,
+		Country:  []string{"US"},
+		Platform: []string{"ios"},
+		Gender:   []string{"M"},
+	})
+	s.NoError(err)
 }
 
-func TestGetAd(t *testing.T) {
-	// db, mock, err := sqlmock.New()
-	// if err != nil {
-	// 	t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
-	// }
-	// defer db.Close()
-
-	// rows := sqlmock.NewRows([]string{"id", "title", "start_at", "end_at"}).
-	// 	AddRow(1, "test", "2021-01-01", "2021-01-02")
-	// mock.ExpectQuery("SELECT id, title, start_at, end_at FROM ads WHERE id = ?").
-	// 	WithArgs(1).
-	// 	WillReturnRows(rows)
-
-	// r := NewAdRepository(db)
-	// ad, err := r.GetAd(1)
-	// if err != nil {
-	// 	t.Errorf("error was not expected while updating stats: %s", err)
-	// }
-	// if ad.ID != 1 {
-	// 	t.Errorf("expected id to be 1, but got %d", ad.ID)
-	// }
-	// if ad.Title != "test" {
-	// 	t.Errorf("expected title to be test, but got %s", ad.Title)
-	// }
-	// if ad.StartAt != "2021-01-01" {
-	// 	t.Errorf("expected start_at to be 2021-01-01, but got %s", ad.StartAt)
-	// }
-	// if ad.EndAt != "2021-01-02" {
-	// 	t.Errorf("expected end_at to be 2021-01-02, but got %s", ad.EndAt)
-	// }
+func TestRepository(t *testing.T) {
+	suite.Run(t, new(repositorySuite))
 }
--- backend/go.mod
+++ backend/go.mod
@@ -3,27 +3,30 @@ module github.com/huhuhu0420/simple-ad-service
 go 1.21.7
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.5.2
 	github.com/gin-contrib/cors v1.5.0
 	github.com/gin-gonic/gin v1.9.1
 	github.com/lib/pq v1.10.9
 	github.com/sirupsen/logrus v1.9.3
 	github.com/spf13/viper v1.18.2
+	github.com/stretchr/testify v1.8.4
 	github.com/swaggo/swag v1.16.3
 )
 
 require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/go-openapi/jsonpointer v0.20.2 // indirect
 	github.com/go-openapi/jsonreference v0.20.4 // indirect
 	github.com/go-openapi/spec v0.20.14 // indirect
 	github.com/go-openapi/swag v0.22.9 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	golang.org/x/tools v0.18.0 // indirect
 )
 
 require (
-	github.com/DATA-DOG/go-sqlmock v1.5.2
 	github.com/bytedance/sonic v1.10.1 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
 	github.com/chenzhuoyu/iasm v0.9.0 // indirect
