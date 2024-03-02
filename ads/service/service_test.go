96e66aeff271b7831d3931a54112f66991ca11b9
huhuhu0420 <ghostcheng.00@gmail.com>
2024-03-02 15:27:58 +0800

    Refactor ad creation with multiple conditions in service_test

--- ads/service/service_test.go
+++ ads/service/service_test.go
@@ -22,22 +22,42 @@ func (s *serviceSuite) SetupSuite() {
 func (s *serviceSuite) TestCreateAd() {
 	s.repository.On("InsertNewAd", "test", "2021-01-01", "2021-01-02").Return(1, nil)
 	s.repository.On("InsertAgeRange", 1, 10, 20).Return(nil)
-	s.repository.On("InsertCountry", 1, []string{"US"}).Return(nil)
-	s.repository.On("InsertPlatform", 1, []string{"ios"}).Return(nil)
+	s.repository.On("InsertCountry", 1, []string{"US", "TW", "JP"}).Return(nil)
+	s.repository.On("InsertPlatform", 1, []string{"ios", "web"}).Return(nil)
 	s.repository.On("InsertGender", 1, []string{"M"}).Return(nil)
 
-	err := s.service.CreateAd(domain.AdInfo{
+	testCase := []domain.AdInfo{}
+	// ad has conditions
+	testCase = append(testCase, domain.AdInfo{
 		Title:   "test",
 		StartAt: "2021-01-01",
 		EndAt:   "2021-01-02",
-	}, domain.Conditions{
-		AgeStart: 10,
-		AgeEnd:   20,
-		Country:  []string{"US"},
-		Platform: []string{"ios"},
-		Gender:   []string{"M"},
+		Conditions: domain.Conditions{
+			AgeStart: 10,
+			AgeEnd:   20,
+			Country:  []string{"US", "TW", "JP"},
+			Platform: []string{"ios", "web"},
+			Gender:   []string{"M"},
+		},
 	})
-	s.NoError(err)
+	// ad has no conditions
+	testCase = append(testCase, domain.AdInfo{
+		Title:   "test",
+		StartAt: "2021-01-01",
+		EndAt:   "2021-01-02",
+		Conditions: domain.Conditions{
+			AgeStart: 0,
+			AgeEnd:   0,
+			Country:  []string{},
+			Platform: []string{},
+			Gender:   []string{},
+		},
+	})
+
+	for _, tc := range testCase {
+		err := s.service.CreateAd(tc, tc.Conditions)
+		s.NoError(err)
+	}
 }
 
 func (s *serviceSuite) TestGetAd() {
